8b57434dd18186020459d1abdfbe0fa72d8cb2de
ethan <190196539@qq.com>
2016-08-17 14:38:34 +0800

    wxpay-demo

---
+++ demo/wxpay.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$config = array(
+    'appId' => 'wx00e5904efes9',
+    'appSecret' => '419ffc73eb1fga846c6b04b',
+    'token' => '54b780cbe9047',
+    'encodingAesKey' => 'cz7NR8g4vk5yMydpG3g8amyFGbjavCuR33',
+    'mchId' => '1220633201',
+    'apiSecret' => 'b4944959c6eaed319a86e3a10d',
+);
+
+// 初始化SDK
+\PFinal\Wechat\Kernel::init($config);
+
+// 获取OpenID
+$openid = \PFinal\Wechat\Service\OAuthService::getOpenid();
+//$openid='oU3OCt5O46PumN7IE87WcoYZY9r0';
+
+// JS签名
+$signPackage = \PFinal\Wechat\Service\JsService::getSignPackage();
+
+// 支付成功通知url
+$notifyUrl = 'http://xxx.com/index.php/wxpay/notify';
+
+// 订单信息
+$order = array(
+    'totalFee' => 0.01,
+    'tradeNo' => uniqid(),
+    'name' => '测试订单',
+);
+
+// 业务签名
+$bizPackage = \PFinal\Wechat\Service\PayService::createJsBizPackage(
+    $openid, $order['totalFee'], $order['tradeNo'], $order['name'], $notifyUrl, $signPackage['timestamp']);
+
+// 加载视图
+return \View::render('pay.twig', array(
+    'openid' => $openid,
+    'signPackage' => $signPackage,
+    'bizPackage' => $bizPackage,
+));
