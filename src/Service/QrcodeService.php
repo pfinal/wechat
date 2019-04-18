a3efdca8d5532489e6822058c3cd37230dbfd7a0
Ethan <nicesoft333@qq.com>
2019-04-18 11:41:10 +0800

    小程序二维码

--- src/Service/QrcodeService.php
+++ src/Service/QrcodeService.php
@@ -110,7 +110,7 @@ class QrcodeService extends BaseService
      *
      * @param string $page 必须是已经发布的小程序存在的页面（否则报错），例如 pages/index/index, 根路径前不要填加 /,不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面
      * @param string $scene 最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
-     * @return mixed 直接返回图片二进制内容 如果要输出到浏览器，可以加上 header('Content-type: image');
+     * @return mixed 直接返回图片二进制内容 如果要输出到浏览器，可以加上 header('Content-type: image/jpeg');
      * @throws WechatException
      */
     public static function miniProgramQrcode($page, $scene)
