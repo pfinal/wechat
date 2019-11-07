61bdf503b4f6bc8903a96a71d0ca01c3f62ab69f
Yanlong-li <ahlyl94@gmail.com>
2019-11-07 18:53:41 +0800

    编排子扩展结构，方便后期拆分。

---
+++ src/pay/Pay.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *   Copyright (c) [2019] [Yanlongli <jobs@yanlongli.com>]
+ *   [Wechat] is licensed under the Mulan PSL v1.
+ *   You can use this software according to the terms and conditions of the Mulan PSL v1.
+ *   You may obtain a copy of Mulan PSL v1 at:
+ *       http://license.coscl.org.cn/MulanPSL
+ *   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ *   PURPOSE.
+ *   See the Mulan PSL v1 for more details.
+ *
+ *   Author: Yanlongli <jobs@yanlongli.com>
+ *   Date:   2019/11/7
+ *   IDE:    PhpStorm
+ *   Desc:   微信支付
+ */
+
+namespace yanlongli\wechat\pay;
+
+/**
+ * Class Pay
+ * @package yanlongli\wechat\pay
+ */
+class Pay
+{
+
+}
